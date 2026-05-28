24fd0173eb0bfbbe66dd383a21feee3f0b35c8d6
BayuAlif <bayualif170505@gmail.com>
2026-05-29 01:35:10 +0700

    no returning work_order with pending status

--- backend/app/Http/Controllers/kelolaWorkOrderController.php
+++ backend/app/Http/Controllers/kelolaWorkOrderController.php
@@ -24,6 +24,7 @@ class kelolaWorkOrderController extends Controller
             'mekanik',
             'logs'
         ])
+        ->where('statusWO', '!=', 'pending')
         ->whereHas('booking', function ($query) use ($user) {
             $query->where(
                 'bengkel_id',
