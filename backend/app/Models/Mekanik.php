9eb4db5494d0507b3d26062dd57cdb296f6cbf49
BayuAlif <bayualif170505@gmail.com>
2026-06-12 16:30:09 +0700

    another lowercase problem

--- backend/app/Models/Mekanik.php
+++ backend/app/Models/Mekanik.php
@@ -7,7 +7,7 @@ use App\Models\work_order;
 
 class Mekanik extends Model
 {
-    protected $table = 'Mekanik';
+    protected $table = 'mekanik';
 
     protected $fillable = [
         'kodeMekanik',
