f52cb3f08611a503d853b858ffd00c9b11fe9dde
BayuAlif <bayualif170505@gmail.com>
2026-06-12 16:22:31 +0700

    lowercase fix on calling mekanik model

--- backend/app/Http/Controllers/MekanikController.php
+++ backend/app/Http/Controllers/MekanikController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Mekanik;
+use App\Models\mekanik;
 use App\Models\Bengkel;
 use Illuminate\Validation\Rule;
 use Illuminate\Support\Facades\Auth;
